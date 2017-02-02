66110e56922e9e88ed3dcd4692b4e9e50207dff8
Thomas Spanner <thomas.spanner@gmx.de>
2017-02-03 00:28:07 +0100

    Remove target

---
+++ src/main/java/de/tomahawk/GithubStatsApplication.java
@@ -0,0 +1,72 @@
+package de.tomahawk;
+
+import java.net.URL;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import de.tomahawk.model.Developer;
+import de.tomahawk.model.Repo;
+import de.tomahawk.service.DeveloperService;
+import de.tomahawk.service.RepoService;
+
+@SpringBootApplication
+public class GithubStatsApplication {
+	
+	public static final String COMPANY = "codecentric";
+	public static final String DEVELOPERS_URL = "https://api.github.com/orgs/" + COMPANY + "/members?per_page=100";
+	
+	private static final Logger log = LoggerFactory.getLogger(GithubStatsApplication.class);
+	
+	public static void main(String[] args) {
+		SpringApplication.run(GithubStatsApplication.class, args);
+	}
+	
+	@Autowired
+	RepoService repoService;
+	
+	@Autowired
+	DeveloperService developerService;
+	
+	@Bean
+	public CommandLineRunner importDevelopers() {
+		return (args) -> {
+			ObjectMapper mapper = new ObjectMapper();
+			
+			List<Developer> developers = mapper.readValue(new URL(DEVELOPERS_URL), new TypeReference<List<Developer>>(){});
+			
+//			int i = 0; // TODO raus
+
+			for (Developer developer : developers) {
+//				if (i > 4) break; TODO raus
+				final String USER_REPOS_URL = developer.getReposUrl() + "?per_page=100";
+				
+				int reposSize = 0;
+				if (developer.getReposUrl() != null) {
+					List<Repo> repos = mapper.readValue(new URL(USER_REPOS_URL),  new TypeReference<List<Repo>>(){});
+					
+					if (repos != null) {
+						for (Repo r : repos) {
+							r.setDeveloper(developer.getLogin());
+							repoService.saveRepo(r);
+						}
+						reposSize = repos.size();
+					}
+				}
+				developer.setRepoCount(reposSize);
+				log.info(developer.getLogin() + " saved with " + reposSize + " repositories");
+				developerService.saveDeveloper(developer);
+//				i = i + 1;
+			}
+		};
+	}
+}
